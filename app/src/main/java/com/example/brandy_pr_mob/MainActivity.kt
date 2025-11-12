4b0f9e6f4d499077dd7bd3c03ced91e36307149e
Bakhitarose <Bakhita.Okong'o@strathmore.edu>
2025-11-12 08:04:40 +0300

    Login page frontend

--- app/src/main/java/com/example/brandy_pr_mob/MainActivity.kt
+++ app/src/main/java/com/example/brandy_pr_mob/MainActivity.kt
@@ -3,18 +3,31 @@ package com.example.brandy_pr_mob
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val output: TextView = findViewById(R.id.output)
+        val username: EditText = findViewById(R.id.username)
+        val password: EditText = findViewById(R.id.password)
+        val loginbtn: Button = findViewById(R.id.loginbtn)
+
+        loginbtn.setOnClickListener {
+            val usernameText = username.text.toString()
+            if (password.text.toString() == "password") {
+                output.text = "Welcome $usernameText"
+
+            } else {
+                output.text = "Incorrect Credentials"
+            }
+        }
+
         }
     }
-}
