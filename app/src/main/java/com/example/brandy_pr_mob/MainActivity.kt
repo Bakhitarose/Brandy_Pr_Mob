fbb78762915ac12f5ebc2497bb2309d1c236eada
Richard Kituyi <richard.junior@strathmore.edu>
2025-11-13 20:49:25 +0300

    fixed errors

--- app/src/main/java/com/example/brandy_pr_mob/MainActivity.kt
+++ app/src/main/java/com/example/brandy_pr_mob/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.brandy_pr_mob
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import android.widget.EditText
+import android.widget.Button
 
-class  MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val welcomeMessage: TextView = findViewById(R.id.welcomeMessage)
+        val output: TextView = findViewById(R.id.output)
+        val username: EditText = findViewById(R.id.username)
+        val password: EditText = findViewById(R.id.password)
+        val loginbtn: Button = findViewById(R.id.loginbtn)
 
-        // Get the username passed from MainActivity
-        val username = intent.getStringExtra("USERNAME")
-        welcomeMessage.text = "Welcome to the Homepage, $username!"
+        loginbtn.setOnClickListener {
+            val usernameText = username.text.toString()
+            if (password.text.toString() == "password") {
+                output.text = "Welcome $usernameText"
+
+                //Connecting with HomeActivity file
+                val intent = Intent(this, HomeActivity::class.java)
+                // Pass the username to HomeActivity
+                intent.putExtra("USERNAME", usernameText)
+                startActivity(intent)
+                finish()
+            } else {
+                output.text = "Incorrect Credentials"
+            }
+        }
+
     }
 }
