bf29347fd0ab0f6808e5c48f24fb487c930a812b
Bakhitarose <Bakhita.Okong'o@strathmore.edu>
2025-11-14 17:08:27 +0300

    Reapply "fragmentation and navigation bar done"
    This reverts commit f29edf2594e46b325e75e624fcb7c6640ecc3dee.

--- app/src/main/java/com/example/brandy_pr_mob/HomeActivity.kt
+++ app/src/main/java/com/example/brandy_pr_mob/HomeActivity.kt
@@ -1,18 +1,69 @@
 package com.example.brandy_pr_mob
 
+import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import com.example.brandy_pr_mob.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
 
-class HomeActivity : AppCompatActivity() {
+
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val welcomeMessage: TextView = findViewById(R.id.welcomeMessage)
+        setSupportActionBar(binding.toolbar)
 
-        // Get the username passed from MainActivity
-        val username = intent.getStringExtra("USERNAME")
-        welcomeMessage.text = "Welcome to the Homepage, $username!"
+        val toggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.toolbar,
+            R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        fragmentManager = supportFragmentManager
+        openFragment(DashboardFragment())
+
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_dashboard -> openFragment(DashboardFragment())
+            R.id.nav_clients -> openFragment(ClientsFragment())
+            R.id.nav_projects -> openFragment(ProjectsFragment())
+            R.id.nav_tasks -> openFragment(TasksFragment())
+            R.id.nav_reports -> openFragment(ReportsFragment())
+            R.id.nav_invoice -> openFragment(InvoiceFragment())
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
